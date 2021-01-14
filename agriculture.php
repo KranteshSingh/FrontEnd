d296ae21f5dbba90b640d1074eb32086a6bb50d2
Krantesh Singh <34528823+KranteshSingh@users.noreply.github.com>
2021-01-14 09:14:49 +0530

    Content Tweaking & Design Alignments

--- agriculture.php
+++ agriculture.php
@@ -18,7 +18,6 @@
     <!--CSS for Hero Section -->
     <link rel="stylesheet" href="assets/vendor/dzsparallaxer/dzsparallaxer.css">
     <link rel="stylesheet" href="assets/vendor/slick-carousel/slick/slick.css">
-
     <link rel="stylesheet" href="assets/vendor/aos/dist/aos.css">
     <!-- CSS Front -->
     <link rel="stylesheet" href="assets/css/theme.css">
@@ -31,32 +30,28 @@
     <?php include 'includes/header.php'; ?>
     <!--Header end-->
     <!-- Hero Section -->
-   
-    
     <div class="container space-top-3">
         <div class="row justify-content-lg-between align-items-lg-center">
             <div class="col-md-6">
                 <div class="mb-2">
-                    <h1 class="display-5 mb-3 text-primary">
-                        Precision Agriculture
+                    <h1 class="display-6 mb-3 text-primary">
+                        Precision Agriculture for
                         <br>
                         <span class="text-primary font-weight-bold">
                             <span class="js-text-animation"></span>
                         </span>
                     </h1>
-                    <p class="lead"> Yuktix IoT devices provide accurate  
-                    micro weather, soil and environment data for farm digitization. 
-                    Our greensense app provides data backed soil health, 
-                    irrigation and crop management advisory to ensure more profits 
-                    per acre of land. We would love to hear from you.
+                    <p class="lead"> Yuktix IoT devices provide accurate
+                        micro weather, soil and environment data for farm digitization.
+                        Our greensense app provides data backed soil health,
+                        irrigation and crop management advisory to ensure more profits
+                        per acre of land. We would love to hear from you.
                 </div>
                 <div class="d-sm-flex align-items-sm-center flex-sm-wrap">
                     <a class="btn btn-primary transition-3d-hover mb-2" href="contact.php">Contact Us</a>
                     <div class="mx-2"></div>
-                    <a class="btn btn-primary transition-3d-hover mb-2" href="#">Download Brochures</a>
+                    <a class="btn btn-primary transition-3d-hover mb-2" href="downloads.php">Download Brochures</a>
                 </div>
-              
-
             </div>
             <div class="col-sm-10 col-lg-6 mb-0 mb-lg-0">
                 <img class="img-fluid rounded" src="assets/media/images/agriculture/Frame6.jpg"
@@ -65,28 +60,31 @@
         </div>
     </div>
     <!-- End Hero Section -->
-    
+    <br>
+    <br>
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-md-8 text-center">
                 <div>
-                    <div class="w-md-75 w-lg-70 text-center mx-md-auto mb-3 mb-md-9 mt-2">
-                        <h2 class="display-5 text-primary">A second generation farmer speaks</h2>
+                    <div class="w-md-75 w-lg-70 text-center mx-md-auto mb-1 mb-md-5 mt-2">
+                        <h2 class="display-5 text-primary">Lets Hear, What Shankar has to say as a Modern Farmer</h2>
                     </div>
                     <p>
-                    His family has been in large scale farming of taiwanese guavas and other produce for the past 25 years. 
-                    To him, every sapling on his farm is like a child that needs to be nurtured and cared for. That is why it is 
-                    important to get an honest feedback from Shankar about our technologies.  
+                        His family has been in large scale farming of taiwanese guavas and other produce for the past 25
+                        years.
+                        To him, every sapling on his farm is like a child that needs to be nurtured and cared for. That
+                        is why it is
+                        important to get an honest feedback from Shankar about our technologies.
                     </p>
+                    <br>
                     <div class="embed-responsive embed-responsive-16by9 rounded"><iframe class="embed-responsive-item"
-                            width="920" height="450" src="https://www.youtube.com/embed/QNXhiP2UhsY" frameborder="0"
+                            width="950" height="450" src="https://www.youtube.com/embed/QNXhiP2UhsY" frameborder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowfullscreen></iframe></div>
                 </div>
             </div>
         </div>
     </div>
-
     <section id="details" class="details">
         <div class="container">
             <div class="row content align-items-lg-center">
@@ -96,7 +94,6 @@
                 <div class="col-md-8" data-aos="fade-up">
                     <h3>Greensense - Your own digital agronomist</h3>
                     <p>
-
                         India is behind US, china and other european countries in terms of productivity / acre. The
                         reason behind this is un-availibilty of scitific advisory backed by data. This leads to low
                         productivity.
@@ -120,17 +117,16 @@
                 <div class="col-md-8 order-2 order-md-1" data-aos="fade-up">
                     <h3>Soil intelligence</h3>
                     <p>
-                    The amount of soil moisture available at the root zone can decide
-                    the health of the crop. Accurate soil moisture can help you avoid water stress 
-                    and ensure that the plants are healthy to provide more profits. 
-                    
-                    Yuktix provides accurate soil moisture devices to detect soil 
-                    volumetric water content at multiple depths to predict water 
-                    stress and send irrigation advisory. With our award winning wireless 
-                    technology, you can easily create a network of soil moisture sensors 
-                    that run on solar power.
-
-
+                        The amount of soil moisture available at the root zone can decide
+                        the health of the crop. Accurate soil moisture can help you avoid water stress
+                        and ensure that the plants are healthy to provide more profits.
+                        <br>
+                        <br>
+                        Yuktix provides accurate soil moisture devices to detect soil
+                        volumetric water content at multiple depths to predict water
+                        stress and send irrigation advisory. With our award winning wireless
+                        technology, you can easily create a network of soil moisture sensors
+                        that run on solar power.
                     </p>
                     <a href="smart-irrigation.php">Yuktix Soil Moisture Sensor</a> | <a
                         href="table-grape-grower-in-karnataka.php">Case Study</a>
@@ -150,7 +146,6 @@
                     <p> With Yuktix next generation intelligence software, now get notificed in advance about
                         possibility of disease and spray accordingly. Save upto 25% on pesticie and gorw produce with
                         less residue</p>
-
                     <a href="greensense.php">Yuktix Greensense</a> | <a href="table-grape-grower-in-karnataka.php">Case
                         Study</a>
                 </div>
@@ -159,12 +154,8 @@
             <br>
         </div>
     </section><!-- End Details Section -->
-
-    
     <!-- ======= Case Study Section ======= -->
-   
-
-    <?php include 'includes/footer.php';?>
+    <?php include 'includes/footer.php'; ?>
     <!-- Global Scripts-->
     <script src="assets/vendor/jquery/dist/jquery.min.js"></script>
     <script src="assets/vendor/jquery-migrate/dist/jquery-migrate.min.js"></script>
